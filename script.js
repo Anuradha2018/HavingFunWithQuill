669bd14745df579449ad92ef55791ed6f18d75c9
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2019-01-20 23:26:50 +0100

    adding new functionalities

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 let toolbarOptions = [
     ['bold', 'italic', 'underine', 'strike'],
-    ['blockquote', 'code-block']
+    ['blockquote', 'code-block'],
+    [{'header': [1,2.3,4,5,6,false]}],
+    [{'list':'ordered'}, {'list': 'bullet'}],
+    [ { 'script': 'sub'}, {'script': 'super'}],
+    [{'indent': '-1'},{'indent': '+1'} ],
+    [{'direction': 'rtl'}],
+    [{'size':['small', false, 'large', 'huge']}],
+    ['link', 'image', 'video', 'formula'],
+    [{'color':[]}, {'background':  []}],
+    [{'font': []}],
+    [{'align':[]}],
 ];
-
 let quill = new Quill('#editor', {
     modules: {
         toolbar:toolbarOptions
